440591f6ee85ae9048d8f3e0ed2f99146bea6f31
sujeet <ssujeet6449@gmail.com>
2026-09-22 12:20:18 +0530

    FactorialByRecursion

---
+++ Recursion/FactorialByRecursion.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+int factorial(int n){
+
+    // base case
+    if(n == 0){
+        return 1;
+}
+    //recursive relation 
+    return n * factorial( n-1);
+
+}
+
+int main(){
+
+    int n;
+    cout<<" enter the number "<<endl;
+    cin>>n;
+
+    int ans = factorial(n);
+    cout<<ans<<endl;
+
+return 0;
+}
