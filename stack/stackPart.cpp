3f9ef458974fd3656816107a1f95434e828ee09b
sujeet <ssujeet6449@gmail.com>
2026-09-24 14:34:46 +0530

    StackImplementtaion

---
+++ stack/stackPart.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class stack{
+  //data member
+  int *arr;
+  int top;
+  int size;
+    
+  stack( int size){
+    this -> size =size;
+    int arr = new int[size];
+    int top = -1;
+  }
+
+  
+
+  void push(){
+    
+  }
+
+}
+
+// properties/ behaviour
+
+
+
+
+
+int main(){
+
+
+
+}
